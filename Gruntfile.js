7938445bd8b4e1ef5e4da848baa4edf6fd1b434d
Phil Freeman <paf31@cantab.net>
2014-11-25 19:58:44 -0800

    Rename docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
     pscDocs: {
       lib: {
         src: ["src/**/*.purs"],
-        dest: "docs/README.purs"
+        dest: "docs/README.md"
       }
     },
 
