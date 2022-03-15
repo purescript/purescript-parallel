1f39668d0a5a8a6c4de758634c8d94e9b1cbd167
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-14 17:16:55 -0700

    Replace require with import

--- test/Main.js
+++ test/Main.js
@@ -1,7 +1,9 @@
+import http from "http";
+
 export function getImpl(opts) {
     return function(done) {
         return function() {
-            require('http').request(opts, function(res) {
+            http.request(opts, function(res) {
                 var body = '';
                 res.setEncoding('utf8');
                 res.on('data', function (s) {
